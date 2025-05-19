8d84a9b4254403a48801cd095eddf3957228ade9
kellybarbosaw <kelly.barbosa@aldiweb.com.br>
2025-05-19 14:37:37 -0300

    removendo comentário de enviroment

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
     api: 'https://aldiwebglender.onrender.com',
-    //api:'http://localhost:3000',
     apiCnpj:'/v1/cnpj',
     apiCep:'/ws'  
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
-    // api:'https://localhost:3000',
-    //api:'https://10.0.0.175:3000',
     api: 'https://aldiwebglender.onrender.com',
     apiCnpj:'/v1/cnpj',
     apiCep:'/ws'   
